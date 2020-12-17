f277cb50a5e4f5852dcf9e2d7d16e392ac58e8ed
Amanda Gomez <o0amandagomez0o@gmail.com>
2020-12-17 11:38:44 -0600

    completed sql code

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,29 @@
 use albums_db;
-show create table albums;
+
+describe albums;
+
+select *
+from albums
+where artist = 'Pink Floyd';
+
+select release_date
+from albums
+where name = 'Sgt. Pepper''s lonely Hearts Club Band';
+
+select genre
+from albums
+where name = 'Nevermind';
+
+select *
+from albums
+where release_date between '1990-01-01' and '1999-12-31';
+
+select *
+from albums
+where sales < 20;
+
+# Does not incld Hard Rock or progressive rock bc I specifically asked for Rock only.
+select *
+from albums
+where genre = 'Rock';
+
