18730c83e5552562af0205ef6eb5508d0630670a
Amanda Gomez <o0amandagomez0o@gmail.com>
2020-12-18 12:12:18 -0600

    using concat

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,6 +7,18 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+/*
+Zykh
+Zyda
+Zwicker
+Zweizig
+Zumaque
+Zultner
+Zucker
+Zuberek
+Zschoche
+Zongker
+*/
 
 #3 
 select *
@@ -23,13 +35,20 @@ LIMIT 5;
 # Petter Stroustrup
 
 #4
-select *
+select concat(first_name," ", last_name) AS 'Full Name'
 FROM employees
 WHERE hire_date LIKE '199%'
 	AND
 	  birth_date LIKE '%-12-25'
 ORDER BY hire_date 
 LIMIT 5 OFFSET 45;
+/*
+Pranay Narwekar
+Marjo Farrow
+Ennio Karcich
+Dines Lubachevsky
+Ipke Fontan
+*/
 # The relationship between LIMIT & OFFSET can be viewed as a function: OFFSET = LIMIT*p-LIMIT.
 
 select *
@@ -39,3 +58,5 @@ WHERE hire_date LIKE '199%'
 	  birth_date LIKE '%-12-25'
 ORDER BY hire_date 
 LIMIT 50;
+
+
