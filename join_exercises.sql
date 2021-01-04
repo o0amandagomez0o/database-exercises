36c805201f341a11b39830ff5afa1a0ca2f4de8b
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-01-04 16:06:42 -0600

    completed Join Example DB 1-3 & Employee DB 1-5

---
+++ join_exercises.sql
@@ -0,0 +1,102 @@
+/*JOIN EXAMPLE DATABASE*/
+
+-- #1
+SELECT *
+FROM roles;
+
+SELECT *
+FROM users;
+
+SELECT *
+FROM users
+JOIN roles ON users.role_id = roles.id;
+
+-- #2
+-- Join Ex: excludes NULLs
+SELECT *
+FROM users
+JOIN roles ON users.role_id = roles.id;
+
+-- LEFT Join Ex: includes NULLs on id's 5 & 6, under role_id and name
+SELECT *
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
+
+-- RIGHT Join Ex: includes NULLs for commenter's id/name/email/role_id since there are no commenters 
+SELECT *
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
+-- #3 lists all possible roles and the number of USERS that have that role.
+SELECT roles.name, count(users.name)
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id
+GROUP BY roles.name;
+
+
+
+
+/*EMPLOYEES DATABASE*/
+
+-- 1
+USE employees;
+
+-- 2: shows each department along with the name of the current manager for that department.
+/*
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no;
+#WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;*/
+
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+	JOIN dept_manager AS dm
+		ON dm.emp_no = e.emp_no
+	JOIN departments AS d
+		on d.dept_no = dm.dept_no
+		
+WHERE dm.to_date > curdate()
+ORDER BY d.dept_name;
+
+-- 3: lists the name of all departments currently managed by women.
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+	JOIN dept_manager AS dm
+		ON dm.emp_no = e.emp_no
+	JOIN departments AS d
+		on d.dept_no = dm.dept_no
+	
+WHERE dm.to_date > curdate()
+AND e.gender = 'F'
+ORDER BY d.dept_name;
+
+-- 4: Find the current titles of employees currently working in the Customer Service department.
+SELECT t.title AS 'Title', count(*)
+FROM titles AS t
+	JOIN dept_emp as de
+		ON de.emp_no = t.emp_no
+	WHERE de.to_date > curdate()
+	AND t.to_date > curdate()
+	AND de.dept_no = 'd009'
+GROUP BY t.title;
+
+-- 5: Find the current salary of all current managers.
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+FROM employees AS e
+	JOIN dept_manager AS dm
+		ON dm.emp_no = e.emp_no
+	JOIN departments AS d
+		on d.dept_no = dm.dept_no
+	JOIN salaries AS s
+		ON s.emp_no = dm.emp_no
+		
+WHERE dm.to_date > curdate()
+AND s.to_date > curdate()
+ORDER BY d.dept_name;
+
+
+
