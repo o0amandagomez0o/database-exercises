bed2fd38ad475cfd029a138900f10fc5c81841ce
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-01-04 23:17:29 -0600

    completed Employee DB #6-10 and a start on #11

--- join_exercises.sql
+++ join_exercises.sql
@@ -98,5 +98,99 @@ WHERE dm.to_date > curdate()
 AND s.to_date > curdate()
 ORDER BY d.dept_name;
 
+-- 6: Find the number of current employees in each department.
+SELECT d.dept_no, d.dept_name, COUNT(*)
+FROM dept_emp AS de
+	JOIN departments AS d
+		ON de.dept_no = d.dept_no
+WHERE de.to_date > curdate()
+GROUP BY d.dept_no
+ORDER BY d.dept_no;
+
+-- 7: Which department has the highest average salary? Hint: Use current not historic information.
+SELECT dept_no, avg(salary)
+from salaries
+join dept_emp using (emp_no)
+group by dept_no
+order by avg(salary) desc;
+
+
+SELECT d.dept_name, avg(s.salary) AS 'average_salary'
+FROM departments AS d
+	JOIN dept_emp AS de
+		ON d.dept_no = de.dept_no
+	JOIN salaries AS s
+		ON s.emp_no = de.emp_no
+WHERE s.to_date > curdate()
+AND de.to_date > curdate()
+GROUP BY d.dept_name
+ORDER BY average_salary DESC
+LIMIT 1;
+
+-- 8: Who is the highest paid employee in the Marketing department?
+SELECT e.first_name, e.last_name
+FROM salaries AS s
+	JOIN employees AS e
+		ON s.emp_no = e.emp_no
+	JOIN dept_emp AS de
+		ON s.emp_no = de.emp_no
+WHERE s.to_date > curdate()
+AND de.dept_no = 'd001'
+ORDER BY s.salary DESC
+LIMIT 1;
+		
+-- 9:Which current department manager has the highest salary?
+SELECT e.first_name, e.last_name, s.salary, d.dept_name
+FROM employees AS e
+	JOIN dept_manager AS dm
+		ON dm.emp_no = e.emp_no
+	JOIN departments AS d
+		ON d.dept_no = dm.dept_no
+	JOIN salaries AS s
+		ON s.emp_no = dm.emp_no
+	
+WHERE dm.to_date > curdate()
+AND s.to_date > curdate()
+ORDER BY s.salary DESC
+LIMIT 1;
+
+-- 10:Bonus Find the names of all current employees, their department name, and their current manager's name.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', ewd.dept_name AS 'Department Name', CONCAT(ewd.first_name, ' ', ewd.last_name) AS 'Manager Name'
+		
+FROM dept_emp AS de
+	JOIN dept_manager AS dm
+		ON de.dept_no = dm.dept_no
+	JOIN employees_with_departments AS ewd
+		ON dm.emp_no = ewd.emp_no
+	JOIN employees AS e
+		ON e.emp_no = de.emp_no
+
+WHERE de.to_date = '9999-01-01'
+AND dm.to_date = '9999-01-01'
+ORDER BY ewd.dept_name, e.last_nam, e.first_name;  
+
+-- 11: Bonus Who is the highest paid employee within each department.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', ewd.dept_name AS 'Department Name', s.salary
+		
+FROM dept_emp AS de
+	JOIN dept_manager AS dm
+		ON de.dept_no = dm.dept_no
+	JOIN employees_with_departments AS ewd
+		ON dm.emp_no = ewd.emp_no
+	JOIN employees AS e
+		ON e.emp_no = de.emp_no
+	JOIN salaries AS s 
+		ON s.emp_no  = e.emp_no
+
+WHERE de.to_date = '9999-01-01'
+AND dm.to_date = '9999-01-01'
+AND s.to_date > NOW()
+#AND e.emp_no NOT IN (111939, 110567, 110114, 110228, 110039, 110420, 110854, 111534, 111133)
+ORDER BY ewd.dept_name, s.salary DESC;
+
+
+
+
+
 
 
