c395b3f6ed48e3b034069eec3724434a6b6fe576
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-01-12 09:21:31 -0600

    morning attempt at #13

--- more_exercises.sql
+++ more_exercises.sql
@@ -839,6 +839,20 @@ WHERE title = "Hunchback Impossible";
 
 
 -- 13.The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+-- view table
+SELECT *
+FROM film;
+
+-- filter titles
+SELECT *
+FROM film
+WHERE title LIKE 'K%';
+
+-- FILTER TITLES
+SELECT *
+FROM film
+WHERE title LIKE 'K%'
+	OR title LIKE 'Q%';
 
 
 -- 14.Use subqueries to display all actors who appear in the film Alone Trip.
